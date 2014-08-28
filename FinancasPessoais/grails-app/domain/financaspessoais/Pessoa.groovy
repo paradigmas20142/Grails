ab9706fa54c31749d2cf3d2a5ff4ba40579eba47
carol15022 <carol15022@hotmail.com>
2014-08-28 20:54:44 -0300

    Estrutura Inicial e Classe Pessoa

---
+++ FinancasPessoais/grails-app/domain/financaspessoais/Pessoa.groovy
@@ -0,0 +1,15 @@
+package financaspessoais
+
+class Pessoa {
+
+	String nome
+	int telefone
+	String endereco
+	String sitFinanceira
+	int salario
+
+	float calcSalarioAnual (){}
+	
+    static constraints = {
+    }
+}
